de35f62f5b5c7c588ae98b1352b00735bf655697
Estelle <elepeigneux@gmail.com>
2016-06-01 16:29:52 +0200

    ajout bloc image sans legende

---
+++ app/technologie-bloc/technologie-bloc.component.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Estelle on 31/05/2016.
+ */
+'use strict';
+
+angular.
+module('technologieBloc').
+component('technologieBloc', {
+    templateUrl: 'technologie-bloc/technologie-bloc.template.html',
+    controller: function technologieBlocController() {
+        
+    }
+});

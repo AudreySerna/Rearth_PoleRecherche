ed89d21a66b966d9975f9b83fc78fecccc2f1551
Estelle <elepeigneux@gmail.com>
2016-06-20 13:52:53 +0200

    ajout template rectangle

---
+++ app/accueil/technologie-rectangle/technologie-rectangle.component.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Estelle on 31/05/2016.
+ */
+'use strict';
+
+angular.
+module('technologieRectangle').
+component('technologieRectangle', {
+    templateUrl: 'accueil/technologie-rectangle/technologie-rectangle.template.html',
+    controller: function technologieRectangleController() {
+        
+    }
+});
